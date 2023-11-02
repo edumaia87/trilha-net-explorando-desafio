695488cf2369183aefe06f666ad38c2f4bfe62ff
Eduardo Maia <edumaia89@outlook.com>
2023-11-02 10:18:58 -0300

    initial commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da súite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor = 
+                    (DiasReservados * Suite.ValorDiaria) - (DiasReservados * Suite.ValorDiaria * 0.1M);
+            }
+
+            return valor;
+        }
+    }
+}
